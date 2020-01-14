92b0a6a01ace77aa884b2a8494a949106b257277
Ilia Lesik <ilialesik@gmail.com>
2020-01-14 20:39:15 +0300

    Make DefaultBackground multiplatform

--- src/components/flash/index.stories.tsx
+++ src/components/flash/index.stories.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import Flash from "./Flash";
 import {storiesOf} from "@storybook/react-native";
 import {boolean, withKnobs, text} from "@storybook/addon-knobs";
+import {defaultBackground} from "../storybook-decorators/DefaultBackground";
 
 
 storiesOf('Flash', module)
     .addDecorator(withKnobs)
+    .addDecorator(defaultBackground)
     .add('Flash', () => <Flash
         isWarn={boolean("isWarn", false)}
         isError={boolean("isError", false)}
--- src/components/storybook-decorators/DefaultBackground.tsx
+++ src/components/storybook-decorators/DefaultBackground.tsx
@@ -1,4 +1,14 @@
 import * as React from "react";
-import "./default-background.css"
+import {StyleSheet, View} from "react-native";
 
-export const defaultBackground = story => <div className="default-background ">{story()}</div>
+export const styles = StyleSheet.create({
+    main: {
+        padding: 50,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
+
+export const defaultBackground = story => <View style={styles.main}>{story()}</View>

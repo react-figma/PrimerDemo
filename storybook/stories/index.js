873f905ff2016a15cd7e598dfb5f6aeffb8875f9
Ilia Lesik <ilialesik@gmail.com>
2020-01-14 15:42:46 +0300

    Configure knobs storybook addon

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -6,10 +6,15 @@ import { linkTo } from '@storybook/addon-links';
 import Button from './Button';
 import Welcome from './Welcome';
 import Flash from "../../src/components/flash/Flash";
+import {boolean, text, withKnobs} from "@storybook/addon-knobs";
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Flash', module)
-    .add('Flash', () => <Flash>
-        Flash message goes here
+    .addDecorator(withKnobs)
+    .add('Flash', () => <Flash
+        isWarn={boolean("isWarn", false)}
+        isError={boolean("isError", false)}
+        isSuccess={boolean("isSuccess", false)}>
+        {text("children", "Flash message goes here.")}
     </Flash>);

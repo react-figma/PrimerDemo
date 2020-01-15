1e762e30b4a2258c3ef32f9c26fba74d887ff7e5
Ilia Lesik <ilialesik@gmail.com>
2020-01-15 12:06:56 +0300

    Configure @storybook/addon-actions

--- src/components/primary-button/index.stories.tsx
+++ src/components/primary-button/index.stories.tsx
@@ -3,12 +3,12 @@ import PrimaryButton from "./PrimaryButton";
 import {storiesOf} from "@storybook/react-native";
 import {boolean, withKnobs, text} from "@storybook/addon-knobs";
 import {defaultBackground} from "../storybook-decorators/DefaultBackground";
-
+import { action } from '@storybook/addon-actions';
 
 storiesOf('Button', module)
     .addDecorator(withKnobs)
     .addDecorator(defaultBackground)
-    .add('Primary Button', () => <PrimaryButton
+    .add('Primary Button', () => <PrimaryButton onClick={action('click')}
         isSmall={boolean("isSmall", false)}>
         {text("children", "Primary Button")}
     </PrimaryButton>);
